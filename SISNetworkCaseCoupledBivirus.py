00bab4baa8ca066463e8cae2bc47356a669f21f3
Satvika Anand Raj <59700171+anandsat000@users.noreply.github.com>
2024-10-25 18:00:55 -0400

    Update SISNetworkCaseCoupledBivirus.py

--- SISNetworkCaseCoupledBivirus.py
+++ SISNetworkCaseCoupledBivirus.py
@@ -197,7 +197,7 @@ while t < Tmax and ('I1' in states.values() or 'I2' in states.values() or 'I12' 
 plt.figure(figsize=(12, 6))
 plt.plot(event_times, num_infected1, label='Infected with Virus 1')
 plt.plot(event_times, num_infected2, label='Infected with Virus 2')
-plt.plot(event_times, num_infected12, label='Infected with Virus 2')
+plt.plot(event_times, num_infected12, label='Infected with Virus 1 and 2')
 plt.xlabel('Time')
 plt.ylabel('Number of Infected Nodes')
 plt.title('Networked SIS Process Simulation for Coupled Bivirus')
